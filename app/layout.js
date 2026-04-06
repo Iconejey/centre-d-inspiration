52e3b9a121019c69923576b457647caa4cf3e575
ngouwy <ngouwy@kimpleapp.com>
2026-04-06 11:20:38 +0200

    Set poppins as default font and added a css folder

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { Poppins } from 'next/font/google';
+import './css/globals.css';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin']
+const poppins = Poppins({
+	variable: '--font-poppins',
+	subsets: ['latin'],
+	weight: ['400', '500', '600', '700']
 });
 
 export const metadata = {
@@ -18,8 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="fr" className={`${geistSans.variable} ${geistMono.variable}`}>
-			<body>{children}</body>
+		<html lang="fr" className={`${poppins.variable}`}>
 		</html>
 	);
 }
