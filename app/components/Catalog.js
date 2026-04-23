97b664e94b363d14517e59761d14e34dc05531c4
ngouwy <ngouwy@kimpleapp.com>
2026-04-23 18:45:06 +0200

    Added catalog filter button and cards

--- app/components/Catalog.js
+++ app/components/Catalog.js
@@ -1,14 +1,23 @@
 import '../css/catalog.css';
 import ContestCard from './ContestCard';
+import Image from 'next/image';
 
 export default function Catalog() {
 	return (
 		<section className="catalog">
-			<ContestCard />
-			<ContestCard />
-			<ContestCard />
-			<ContestCard />
-			<ContestCard />
+			<div className="filters">
+				<button className="outlined">
+					Trier par
+					<Image src="/assets/filter.svg" alt="Icone de filtre" width={16} height={16} />
+				</button>
+			</div>
+			<div className="cards">
+				<ContestCard />
+				<ContestCard />
+				<ContestCard />
+				<ContestCard />
+				<ContestCard />
+			</div>
 		</section>
 	);
 }
--- app/components/ContestCard.js
+++ app/components/ContestCard.js
@@ -1,7 +1,19 @@
+import Image from 'next/image';
+
 export default function ContestCard() {
 	return (
 		<div className="contest-card">
-			<h3>Contest Card</h3>
+			<div className="card-img">
+				<Image src="/assets/test-img.png" alt="Image du concours" fill />
+			</div>
+			<div className="content">
+				<span className="title">Pasquier - Partez à l&apos;aventure</span>
+				<div className="mechanic">
+					<Image src="/assets/mechanic-test-icon.svg" alt="Icone de la mécanique" width={18} height={18} />
+					<span className="label">Roue de la chance</span>
+				</div>
+				<button>Voir le jeu</button>
+			</div>
 		</div>
 	);
 }
