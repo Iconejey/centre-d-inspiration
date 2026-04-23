961928089bd6284ff9c5e58599b062ba2d2fb49a
ngouwy <ngouwy@kimpleapp.com>
2026-04-23 17:18:53 +0200

    Implemented sidebar with lottie

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,5 +1,46 @@
+'use client';
+
+import dynamic from 'next/dynamic';
 import '../css/sidebar.css';
+import kbotAnimation from '../kbot-lottie.json';
+
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 
 export default function Sidebar() {
-	return <aside className="sidebar">hello sidebar</aside>;
+	return (
+		<aside className="sidebar">
+			<label htmlFor="mechanic">Mécanique</label>
+			<select name="mechanic" id="mechanic">
+				<option value="">Sélectionner</option>
+			</select>
+
+			<label htmlFor="objectifs">Objectifs marketing</label>
+			<select name="objectifs" id="objectifs">
+				<option value="">Sélectionner</option>
+			</select>
+
+			<label htmlFor="temps-forts">Temps forts</label>
+			<select name="temps-forts" id="temps-forts">
+				<option value="">Sélectionner</option>
+			</select>
+
+			<label htmlFor="secteur">Secteur d&apos;activité</label>
+			<select name="secteur" id="secteur">
+				<option value="">Sélectionner</option>
+			</select>
+
+			<div className="card">
+				<div className="content">
+					<div className="text">
+						<span className="title">Vous avez un projet de jeu marketing</span>
+						<span className="subtitle">vous souhaitez être accompagné par nos équipes ?</span>
+					</div>
+					<button className="large button">Prendre rendez-vous</button>
+				</div>
+				<div className="lottie">
+					<Lottie animationData={kbotAnimation} loop={true} />
+				</div>
+			</div>
+		</aside>
+	);
 }
