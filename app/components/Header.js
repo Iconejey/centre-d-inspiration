d9d47d0a909b30143742b89d8f0098ab7f5a6c90
Nicolas Gouwy <ngouwy@cassiop.com>
2026-05-29 14:08:22 +0200

    Fixed header's images and input accessibility

--- app/components/Header.js
+++ app/components/Header.js
@@ -22,15 +22,17 @@ export default function Header({ search_value = '', handleFilterChange }) {
 	return (
 		<header className="site-header">
 			<div className="bg-k">
-				<Image src="/assets/K.svg" alt="Kimple" fill priority />
+				<Image src="/assets/K.svg" alt="Logo Kimple court" fill priority />
 			</div>
 			<div className="title">
 				<span>Le centre d&apos;inspiration</span>
-				<Image src="/assets/KimpleLogoFull.svg" alt="Kimple" width={100} height={34} priority />
+				<Image src="/assets/KimpleLogoFull.svg" alt="Logo kimple long" width={100} height={34} priority />
 			</div>
 			<div className="search-bar">
-				<Image src="/assets/search.svg" alt="Search" width={16} height={16} priority />
-				<input type="text" value={search_input_value} placeholder="Nom d'une opération" onChange={event => setSearchInputValue(event.target.value)} />
+				<label htmlFor="search-input" aria-label="Rechercher">
+					<Image src="/assets/search.svg" alt="" width={16} height={16} priority />
+				</label>
+				<input id="search-input" type="text" value={search_input_value} placeholder="Nom d'une opération" onChange={event => setSearchInputValue(event.target.value)} />
 			</div>
 		</header>
 	);
